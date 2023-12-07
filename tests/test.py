707fbe893a73a323848c9427ab29a3b26b17801b
ThorIgor <44617152+ThorIgor@users.noreply.github.com>
2023-12-07 19:02:08 +0200

    tests

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+from src.inference import inference_embeddings, inference_no_embeddings
+from src.train import train_embeddings, train_no_embeddings
+
+
+def test_train_embeddings():
+    train_embeddings("test_data/test_dataset.csv", "test_models_embeddings")
+
+def test_train_no_embeddings():
+    train_no_embeddings("test_data/test_dataset.csv", "test_models_no_embeddings")
+
+def test_inference_embeddings():
+    inference_embeddings("test_data/test_inf_dataset.csv", "test_data/test_inf_output.csv", "test_models_embeddings")
+
+def test_inference_no_embeddings():
+    inference_no_embeddings("test_data/test_inf_dataset.csv", "test_data/test_inf_output.csv", "test_models_embeddings")
+
