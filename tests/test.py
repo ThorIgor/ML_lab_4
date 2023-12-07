29f3bdb82319f391392988e646839644dae9626c
ThorIgor <44617152+ThorIgor@users.noreply.github.com>
2023-12-07 21:23:08 +0200

    add test data

--- tests/test.py
+++ tests/test.py
@@ -3,20 +3,20 @@ from src.train import train_embeddings, train_no_embeddings
 
 
 def test_train_embeddings():
-    train_embeddings("test_data/test_dataset.csv", "test_models_embeddings")
+    train_embeddings("tests/test_data/test_dataset.csv", "tests/test_models_embeddings")
 
 def test_train_embeddings_with_embeddings_file():
-    train_embeddings("test_data/test_dataset.csv", "test_models_embeddings", "test_data/test_embeddings.npy")
+    train_embeddings("tests/test_data/test_dataset.csv", "tests/test_models_embeddings", "tests/test_data/test_embeddings.npy")
 
 def test_train_no_embeddings():
-    train_no_embeddings("test_data/test_dataset.csv", "test_models_no_embeddings")
+    train_no_embeddings("tests/test_data/test_dataset.csv", "tests/test_models_no_embeddings")
 
 def test_inference_embeddings():
-    inference_embeddings("test_data/test_inf_dataset.csv", "test_data/test_inf_output.csv", "test_models_embeddings")
+    inference_embeddings("tests/test_data/test_inf_dataset.csv", "tests/test_data/test_inf_output.csv", "tests/test_models_embeddings")
 
 def test_inference_embeddings_with_embeddings_file():
-    inference_embeddings("test_data/test_inf_dataset.csv", "test_data/test_inf_output.csv", "test_models_embeddings", "test_data/test_inf_embeddings.npy")
+    inference_embeddings("tests/test_data/test_inf_dataset.csv", "tests/test_data/test_inf_output.csv", "tests/test_models_embeddings", "tests/test_data/test_inf_embeddings.npy")
 
 def test_inference_no_embeddings():
-    inference_no_embeddings("test_data/test_inf_dataset.csv", "test_data/test_inf_output.csv", "test_models_no_embeddings")
+    inference_no_embeddings("tests/test_data/test_inf_dataset.csv", "tests/test_data/test_inf_output.csv", "tests/test_models_no_embeddings")
 
